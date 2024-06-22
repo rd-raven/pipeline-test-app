a677cfc9b16909e47819d34bf73daff05765dec2
rd-raven <rd.raven.danish@gmail.com>
2024-06-22 11:02:39 +0100

    spring start

---
+++ pipeline-test-app/src/main/java/com/lancers/pipelinetestapp/PipelineTestAppApplication.java
@@ -0,0 +1,13 @@
+package com.lancers.pipelinetestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipelineTestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipelineTestAppApplication.class, args);
+	}
+
+}
---
+++ pipeline-test-app/src/test/java/com/lancers/pipelinetestapp/PipelineTestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lancers.pipelinetestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipelineTestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
